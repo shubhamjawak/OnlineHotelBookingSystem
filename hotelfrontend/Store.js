569cfb7a228c394eeb058f5893f334b2e0f19922
shubhamjawak <98087549+shubhamjawak@users.noreply.github.com>
2022-04-16 00:22:16 +0530

    Delete hotelfrontend directory

--- hotelfrontend/Store.js
+++
@@ -1,9 +0,0 @@
-import { createStoreHook } from "react-redux";
-import reducer, { Reducer } from "./Reducer";
-import { createStore } from "redux"
-
-
-const initialSt={loggedin: false};
- const mystore=createStore(reducer,initialSt)
-
- export default mystore;
